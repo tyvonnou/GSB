e10b2fa2214098f2df4df710ae7501fc02d11124
Théo Yvonnou <tyvonnou@gmail.com>
2017-09-22 11:12:39 +0200

    Update visiteur
    Modification de l'interface visiteur

--- Ajouterfichefrais.php
+++ Ajouterfichefrais.php
@@ -20,7 +20,7 @@ session_start();
 	<div id="header">
 		<div id="logo">
 
-			<h1><img src="Images/icone.png" alt="" class="icone" /></h1>
+			<h1><img src="icone.png" alt="" class="icone" /></h1> <!-- icone de gsb -->
 
 		</div>
 		<div id="menu">
@@ -55,12 +55,22 @@ session_start();
 
       			<label for="annee">Année : </label>
       			<input id="annee" name="annee" type="text"  size="10" maxlength="20"/>
-
+<br />
             <label for="nbjustificatifs">Nombre de justificatifs : </label>
       			<input id="nbjustificatifs" name="nbjustificatifs" type="text"  size="10" maxlength="20"/>
 <br />
-            <label for="montant">Montant valide : </label>
-      			<input id="montant" name="montant" type="text"  size="10" maxlength="20"/>
+            <label for="montant">Nombre de nuitées : </label>
+      			<input id="nuitee" name="nuitee" type="text"  size="10" maxlength="20"/>
+<br/>      			
+      		<label for="montant">Nombre de repas : </label>
+      			<input id="repas" name="repas" type="text"  size="10" maxlength="20"/>
+<br/>      			
+      		<label for="montant">Nombre d'étapes : </label>
+      			<input id="etape" name="etape" type="text"  size="10" maxlength="20"/>      			
+<br/>      			
+      		<label for="montant">Nombre de kilometres : </label>
+      			<input id="kilometre" name="kilometre" type="text"  size="10" maxlength="20"/>      			
+            
             <?php
           				if (isset($_GET['message']))
           					echo $_GET['message'];
@@ -79,31 +89,88 @@ session_start();
 			</div>
 
 <?php
-	//on recupere les varirable issue du formulaire
+	//on recupere les variable issue du formulaire
 @$mois=$_GET['mois'];
 @$annee=$_GET['annee'];
 @$id=$_SESSION['id'];
 @$nbJustificatifs=$_GET['nbjustificatifs'];
-@$montantValide=$_GET['montant'];
+@$nuitee=$_GET['nuitee'];
+@$repas=$_GET['repas'];
+@$etape=$_GET['etape'];
+@$kilometre=$_GET['kilometre'];
 
 
 
-
-
-  if (!empty($montant)) {
+  if (!empty($nuitee)) {
     include 'connectAD.php';
-
-		$sql = "INSERT INTO fichefrais (idVisiteur, mois, annee, nbJustificatifs, montantValide, DateModif, idEtat)
-            VALUES ('".$id."','".$mois."','".$annee."','".$nbJustificatifs."','".$montantValide."','0','0');";
+		
+		//Recuperation des montants dans la table fraisforfait
+    	$sql = "SELECT montant FROM fraisforfait WHERE id = 'ETP'";
+    	$results = tableSQL($sql);
+    
+    	foreach ($results as $ligne){
+    		$montantetape = $ligne[0];
+    	}
+    
+    	$sql = "SELECT montant FROM fraisforfait WHERE id = 'KM'";
+    	$results = tableSQL($sql);
+    
+    	foreach ($results as $ligne){
+    		$montantkm = $ligne[0];
+    	}
+    
+    	$sql = "SELECT montant FROM fraisforfait WHERE id = 'NUI'";
+    	$results = tableSQL($sql);
+    	
+    	foreach ($results as $ligne){
+    		$montantnuitee = $ligne[0];
+    	}
+    	
+    	$sql = "SELECT montant FROM fraisforfait WHERE id = 'REP'";
+    	$results = tableSQL($sql);
+    	
+    	foreach ($results as $ligne){
+    		$montantrepas = $ligne[0];
+    	}
+    
+		$sql = "INSERT INTO lignefraisforfait (idVisiteur, mois, idFraisForfait, quantite)
+			VALUES ('".$id."','".$mois."','NUI','".$nuitee."')";
 		$result = executeSQL($sql);
-
+		
+		$sql = "INSERT INTO lignefraisforfait (idVisiteur, mois, idFraisForfait, quantite)
+			VALUES ('".$id."','".$mois."','REP','".$repas."')";
+		$result = executeSQL($sql);
+		
+		$sql = "INSERT INTO lignefraisforfait (idVisiteur, mois, idFraisForfait, quantite)
+			VALUES ('".$id."','".$mois."','ETP','".$etape."')";
+		$result = executeSQL($sql);
+		
+		$sql = "INSERT INTO lignefraisforfait (idVisiteur, mois, idFraisForfait, quantite)
+			VALUES ('".$id."','".$mois."','KM','".$kilometre."')";
+		$result = executeSQL($sql);
+		
+		//Calculs des cout des differents frais
+		$coutnuitee = $montantnuitee * $nuitee;
+		$coutrepas = $montantrepas * $repas;
+		$coutetape= $montantetape * $etape;
+		$coutkm= $montantkm * $kilometre;
+		
+		//Calcul du cout total de tous les frais reunis
+		$couttot = $coutnuitee + $coutetape + $coutkm + $coutrepas;
+		
+		$date = date("Y-m-d");
+		
+		$sql = "INSERT INTO fichefrais (idVisiteur, mois, annee, nbJustificatifs, montantValide, DateModif, idEtat)
+            VALUES ('".$id."','".$mois."','".$annee."','".$nbJustificatifs."','".$couttot."','".$date."','0');";
+		$result = executeSQL($sql);
+		
 		if ($result)
-			echo "Ajout realise ";
+			echo "Ajout realise";
 		else
 			echo "Erreur";
 
 	}else
-		echo "Merci de renseigner les informations ci dessus";
+		echo "Merci de renseigner les informations ci-dessus";
 
 ?>
 			</div>
